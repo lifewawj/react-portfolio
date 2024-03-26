b3d55845ce18405f6f1c29149b9795866bf4f727
lifewawj <arevaloaj01@gmail.com>
2024-03-26 07:54:04 -0700

    Added more info and styles to About page and Resume for filler content

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 import colors from "../colors";
 
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
 const H2 = styled.h2`
     font-size: 2rem;
 `
@@ -11,11 +18,11 @@ const P = styled.div`
 // TODO: Add an IMG to About Component and styles
 function About() {
     return(
-        <div>
+        <Wrapper>
             <H2>About Me</H2>
             <img></img>
-            <P>A UCSD Bootcamp Student</P>
-        </div>
+            <P>A Filipino-American that was born and raised in Kodiak, Alaska for most of his life. Until After High-school graduation, he than moved California to explore the new opportunities the world has to offer.</P>
+        </Wrapper>
     )
 }
 
--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -6,6 +6,17 @@ function Resume() {
     return(
         <div>
             <h2>Resume</h2>
+            <h3>Frontend Skills</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+            </ul>
+            <h3>Backend Skills</h3>
+            <ul>
+                <li>APIs</li>
+                <li>Express</li>
+            </ul>
         </div>
     );
 }
