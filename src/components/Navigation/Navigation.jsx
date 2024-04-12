b802c41295798ad766a99d01636b3d20b1c4e039
lifewawj <arevaloaj01@gmail.com>
2024-04-12 04:34:55 -0700

    Added an Active feature for whatever Route the web app is on it hightlights the certain NavLink in the navigation bar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,42 +3,72 @@ import colors from "../colors";
 import { NavLink, Link } from "react-router-dom";
 import { useState } from "react";
 
-const Button = styled.button`
-  color: ${colors.textColor};
-  background-color: transparent;
-  border: none;
-  border-radius: 50px;
-  padding: 0;
-  cursor: pointer;
-  font-size: 1rem;
+// const Button = styled.button`
+//   color: ${colors.textColor};
+//   background-color: transparent;
+//   border: none;
+//   border-radius: 50px;
+//   padding: 0;
+//   cursor: pointer;
+//   font-size: 1rem;
 
-  &:hover {
-    color: ${colors.hoverTextColor};
-  }
-`;
-
+//   &:hover {
+//     color: ${colors.hoverTextColor};
+//   }
+// `;
 
 function Navigation() {
   return (
     <>
-      <NavLink to="/">
-        <Button>About</Button>
+      <NavLink
+        to="/"
+        style={({ isActive }) => {
+          return {
+            color: isActive ? "white" : "white",
+            textDecoration: isActive ? "underline" : "none"
+          };
+        }}
+      >
+        About
       </NavLink>
 
-      <NavLink to="/portfolio">
-        <Button>Portfolio</Button>
+      <NavLink
+        to="/portfolio"
+        style={({ isActive }) => {
+          return {
+            color: isActive ? "white" : "white",
+            textDecoration: isActive ? "underline" : "none"
+          };
+        }}
+      >
+        Portfolio
       </NavLink>
 
-      <NavLink to="/contact">
-        <Button>Contact</Button>
+      <NavLink
+        to="/contact"
+        style={({ isActive }) => {
+          return {
+            color: isActive ? "white" : "white",
+            textDecoration: isActive ? "underline" : "none"
+          };
+        }}
+      >
+        Contact
       </NavLink>
 
-      <NavLink to="/resume">
-        <Button>Resume</Button>
+      <NavLink
+        to="/resume"
+        style={({ isActive }) => {
+          return {
+            color: isActive ? "white" : "white",
+            textDecoration: isActive ? "underline" : "none"
+          };
+        }}
+      >
+        Resume
       </NavLink>
     </>
   );
 }
-
 
 export default Navigation;
